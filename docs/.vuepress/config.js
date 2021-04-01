1f1b3f381cb4f26d28ba0ef7e20a5a070c37aa16
Xiwa <60639473+howard666888@users.noreply.github.com>
2021-04-02 00:53:36 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ const themeConfig = require('./config/themeConfig.js');
 module.exports = {
   theme: 'vdoing', // 使用依赖包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
-  title: "面试问题浓缩总结",
+  title: "Yuhao的学习记录",
   description: '把面试笔记浓缩成简短的几句话，深入原理会贴链接',
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
